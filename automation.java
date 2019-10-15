deed4e37673d23300d916d8319fe336dbd322bc4
avtar-qx <56565478+avtar-qx@users.noreply.github.com>
2019-10-15 14:52:18 -0400

    insert code from eclipse

--- automation.java
+++ automation.java
@@ -1,1 +1,9 @@
-I'll write in my automation code soon.
+public class VehicleInsurance {
+
+		public static void main(String[] args) throws InterruptedException, IOException {
+			
+			//Code below is used to Open Chrome Browser
+			
+			System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+			WebDriver driver=new ChromeDriver();
+			
